17cf2e17c68ca793b4b2b42095519050c37fb1d2
szl.kiev <szl.kiev@gmail.com>
2012-11-28 17:16:06 +0700

    urushi: 4.2 initial bring-up
    Change-Id: I9621fcf0466318c0a235f6d85f9b96b700f20afb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,15 +1,25 @@
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
 -include device/semc/mogami-common/BoardConfigCommon.mk
--include vendor/semc/urushi/BoardConfigVendor.mk
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/urushi/config
+
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := cyanogen_urushi_defconfig
 #TARGET_PREBUILT_KERNEL := device/semc/urushi/prebuilt/kernel
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
+# Sensor
 SENSORS_COMPASS_AK8975 := true
 SENSORS_ACCEL_BMA150_INPUT := true
 SENSORS_ACCEL_BMA250_INPUT := false
 SENSORS_PROXIMITY_APDS9700 := true
 SENSORS_PRESSURE_BMP180 := false
 
+# Assert
 TARGET_OTA_ASSERT_DEVICE := ST18i,ST18a,urushi
+
+# inherit from the proprietary version
+-include vendor/semc/urushi/BoardConfigVendor.mk
